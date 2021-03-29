95b31ff6d64214fc7ba7af6e62af534d09d75a8f
Aliya Merali <merali4@gmail.com>
2021-03-29 16:55:23 -0600

    Add empty? and place_ship methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,17 @@
 class Cell
-  attr_reader :coordinate
-  
+  attr_reader :coordinate, :ship
+
   def initialize(cell_coordinates)
     @coordinate = cell_coordinates
+    @ship = nil
   end
 
+  def empty?
+    @ship == nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
 
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -9,7 +9,27 @@ RSpec.describe Cell do
     it "exists" do
       expect(cell).to be_instance_of(Cell)
     end
+
+    it "returns coordinate attribute" do
+      expect(cell.coordinate).to eq("A3")
+    end
   end
 
+  describe "#empty?" do
+    cell = Cell.new("A3")
 
+    it "returns true for empty upon creation" do
+      expect(cell.empty?).to eq(true)
+    end
+  end
+
+  describe "#place_ship" do
+    cell = Cell.new("A3")
+    ship = Ship.new("Cruiser", 3)
+
+    it "updates cell.ship state to be an instance of ship" do
+      cell.place_ship(ship)
+      expect(cell.ship).to be_instance_of(Ship)
+    end
+  end
 end
