e92aa18e1b75f8b3c5853c90a7eaf6d59b417678
Jamison Griffith <jamison.griffith@gmail.com>
2021-03-29 16:26:25 -0600

    Add sunk method to Ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,15 @@
 class Ship
-attr_reader :name, :length
+attr_reader :name, :length, :health
 
   def initialize(ship_name, ship_length)
     @name = ship_name
     @length = ship_length
+    @health = @length
   end
-  
+
+  # Very succinct way to provide behavior
+  def sunk?
+    @health == 0
+  end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -8,6 +8,22 @@ RSpec.describe Ship do
       expect(ship).to be_instance_of(Ship)
     end
 
+    it 'has accessible attributes' do
+      expect(ship.name).to eq("Cruiser")
+      expect(ship.length).to eq(3)
+      expect(ship.health).to eq(3)
+    end
   end
+
+  describe "#sunk?" do
+    ship = Ship.new("Cruiser", 3)
+
+    it 'returns false when health is greater than zero' do
+      expect(ship.sunk?).to eq(false)
+    end
+
+  end
+
+  
 
 end
