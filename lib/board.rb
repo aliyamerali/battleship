d8afe90d57f0a66bd8ca8e9245639865ef7a45ea
Jamison Griffith <jamison.griffith@gmail.com>
2021-03-30 17:30:07 -0600

    Create coordinates array for initializing board

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,25 @@ class Board
   attr_reader :cells
 
   def initialize
+    # Making column and row dimension as variables
+    # so easier to make dimensions change down the line
+    column_dimension = 4
+    row_dimension = column_dimension
+    rows = "A".."D"
+    columns = 1..column_dimension
+    coordinates = []
+
+    # Iterate over row and column ranges to concatenate and push into array of coordinates
+    rows.each do |row|
+      columns.each do |column|
+        coordinates << row + column.to_s
+      end
+    end
+
+    @cells = Hash.new
+
+
   end
+
 
 end
