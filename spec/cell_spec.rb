7783cfa1632c48cea877dccefbfe2bb8934c5bda
Jamison Griffith <jamison.griffith@gmail.com>
2021-03-29 20:49:43 -0600

    Write fire_upon tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -32,4 +32,25 @@ RSpec.describe Cell do
       expect(cell.ship).to be_instance_of(Ship)
     end
   end
+
+  # These two methods work closely enough together to bundle them in the tests
+  describe "#fired_upon? and #fire_upon" do
+    cell1 = Cell.new("A3")
+    cell2 = Cell.new("B1")
+    ship = Ship.new("Cruiser", 3)
+
+    xit "fired_upon? returns correct boolean" do
+      cell1.fire_upon
+      expect(cell1.fired_upon?).to eq(true)
+      expect(cell2.fired_upon?).to eq(false) # didn't call fire_upon on cell2, so expect false
+    end
+
+    # I would think an if/then statement in fire_upon would invoke #hit if ship is on cell
+    xit "fire_upon reduces ship health" do
+      cell2.place_ship(ship)
+      cell2.fire_upon
+      expect(ship.health).to eq(2)
+    end
+  end
+
 end
