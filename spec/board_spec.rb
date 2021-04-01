4e6d212502f8b1003c62b30d65295119f6d6b304
Aliya Merali <merali4@gmail.com>
2021-03-31 18:41:24 -0600

    Add helper method to check for consecutive coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -38,12 +38,26 @@ RSpec.describe Board do
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    it 'validates that number of coordinates = length of the ship' do
+    xit 'validates that number of coordinates = length of the ship' do
       expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
     end
 
-    it 'validates that number of coordinates = length of the ship' do
+    xit 'validates that number of coordinates = length of the ship' do
       expect(board.valid_placement?(cruiser, ["A3"])).to eq(false)
+    end
+
+    xit 'validates that coordinates are consecutive either in rows or cols' do
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
+    end
+  end
+
+  describe "#check_consecutive" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    it 'returns true for consecutive letters on board' do
+      expect(board.check_consecutive(cruiser, [1, 2, 3])).to eq(true)
     end
   end
 
