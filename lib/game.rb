cc6f4a4afb740789f4716a68cd9294fb631d213c
Aliya Merali <merali4@gmail.com>
2021-04-02 13:23:45 -0600

    Add welcome message method and specs

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+class Game
+
+  def initialize
+  end
+
+  def welcome_message
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    action = gets.chomp
+  end
+
+
+end
