a2c34c3f985f006b157bd48b623299551a85648f
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-03 11:09:47 -0600

    Add generate_ship_possibilities method with test

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_reader :cells
+  attr_reader :cells, :rows, :columns
 
   def initialize
     # Making column and row dimension as variables
--- spec/game-spec.rb
+++ spec/game-spec.rb
@@ -21,8 +21,18 @@ describe Game do
   describe '#random_coordinate_generator' do
     game = Game.new
 
-    it 'randomly selects horizontal or vertical' do
+    xit 'randomly selects horizontal or vertical' do
       expect(game.random_coordinate_generator).to be_between(0, 1)
+    end
+  end
+
+  describe "#generate_ship_possibilities" do
+    game = Game.new
+    cpu_board = Board.new
+    ship = Ship.new("cruiser", 3)
+
+    it 'returns array of arrays' do
+      expect(game.generate_ship_possibilities(cpu_board, ship)[0]).to be_instance_of(Array)
     end
   end
 end
